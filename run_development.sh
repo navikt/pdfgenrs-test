bf0aee80529faf383c848c549e8e00cf117f6874
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2026-05-29 08:46:02 +0200

    Update run_development.sh

--- run_development.sh
+++ run_development.sh
@@ -9,7 +9,7 @@ docker run \
         -v $CURRENT_PATH/data:/app/data \
         -v $CURRENT_PATH/fonts:/app/fonts \
         -v $CURRENT_PATH/resources:/app/resources \
-        -v $CURRENT_PATH/resources:/app/templates/resources \
+        -v $CURRENT_PATH/typst.toml:/app/typst.toml \
         -p 8080:8080 \
         -e DEV_MODE=true \
         -it \
