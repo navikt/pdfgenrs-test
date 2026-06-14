2add906b5c3a928effa3af9a1fafb4703c44de4b
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2026-06-14 17:08:14 +0200

    Update run_development.sh

--- run_development.sh
+++ run_development.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CURRENT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PDFGENRS_IMAGE="${PDFGENRS_IMAGE:-ghcr.io/navikt/pdfgenrs:1.0.1}"
+PDFGENRS_IMAGE="${PDFGENRS_IMAGE:-ghcr.io/navikt/pdfgenrs:1.0.2}"
 
 docker pull "$PDFGENRS_IMAGE"
 docker run \
